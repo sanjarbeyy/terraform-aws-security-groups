8e03721cd8892393f60e18f568ef76cceb89cba9
sanjarbeyy <sanjarbey1991@mail.ru>
2024-01-17 05:13:24 -0500

    update

--- main.tf
+++ main.tf
@@ -9,11 +9,12 @@ resource "aws_security_group" "default" {
     for_each = each.value.ingress_rules != null ? each.value.ingress_rules : []
 
     content {
-      description = ingress.value.description
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
+      description     = ingress.value.description
+      from_port       = ingress.value.from_port
+      to_port         = ingress.value.to_port
+      protocol        = ingress.value.protocol
+      cidr_blocks     = ingress.value.cidr_blocks != null ? ingress.value.cidr_blocks : null
+      security_groups = ingress.value.security_groups != null ? ingress.value.security_groups : null
     }
   }
 
@@ -22,11 +23,12 @@ resource "aws_security_group" "default" {
 
 
     content {
-      description = egress.value.description
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
+      description     = egress.value.description
+      from_port       = egress.value.from_port
+      to_port         = egress.value.to_port
+      protocol        = egress.value.protocol
+      cidr_blocks     = egress.value.cidr_blocks != null ? egress.value.cidr_blocks : null
+      security_groups = egress.value.security_groups != null ? egress.value.security_groups : null
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,21 +3,22 @@ variable "security_groups" {
   type = map(object({
     description = string
     ingress_rules = optional(list(object({
-      description = optional(string)
-      priority    = optional(number)
-
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_blocks = optional(list(string))
+      description     = optional(string)
+      priority        = optional(number)
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string))
+      security_groups = optional(list(string))
     })))
     egress_rules = optional(list(object({
-      description = optional(string)
-      priority    = optional(number)
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_blocks = optional(list(string))
+      description     = optional(string)
+      priority        = optional(number)
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string))
+      security_groups = optional(list(string))
     })))
   }))
   default = {}
